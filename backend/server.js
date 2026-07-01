63931f247f7c8729fe246fb33256907d13ff77b5
Saif Qureshi <saifqureshi@Saifs-MacBook-Air.local>
2026-07-02 01:52:43 +0530

    fix render port binding

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ const connectDB = require('./src/db/db.js')
 connectDB()
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on PORT ${PORT}`);
 });
