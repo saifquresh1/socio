0e778fffae020147127841ff35bc379fd06798b3
Saif Qureshi <saifqureshi@Saifs-MacBook-Air.local>
2026-07-02 01:31:35 +0530

    server file update

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,8 @@ const app = require('./src/app.js')
 const connectDB = require('./src/db/db.js')
 
 connectDB()
-    .then(() => {
-        app.listen(3000, ()=>{
-            console.log('server is running');
-        });
-    })
-    .catch((error) => {
-        console.error('Failed to connect to MongoDB:', error);
-        process.exit(1);
-    });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
